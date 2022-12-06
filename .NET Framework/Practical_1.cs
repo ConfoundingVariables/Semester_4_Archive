c03f57f75a243bd1229858cad096ca4d1ef0a436
ConfoundingVariables <96291264+ConfoundingVariables@users.noreply.github.com>
2022-12-06 06:54:25 +0530

    Added Practical 1 and Removed foo.txt
    Added questions for practical 1

---
+++ .NET Framework/Practical_1.cs
@@ -0,0 +1,5 @@
+//Practical 1a - check if both inputs are greater than or smaller than 10, accept if one is greater than 10 and one is smaller than 10
+//Practical 1b - check if both inputs are greater than 10, if not ask for input again
+//Practical 1c - take info for multiple students and store in an array and print, id, name, course, dob.
+
+Console.WriteLine("Hello Unforgiving Tragedy")
